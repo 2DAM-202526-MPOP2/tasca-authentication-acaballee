8901ccf84dce43413232feff13feae093b57f644
«acaballee» <alexcaballe@iesebre.com>
2026-01-08 17:18:30 +0100

    estuctura inicial

--- lib/data/models/user.dart
+++ lib/data/models/user.dart
@@ -1,13 +1,31 @@
 class User {
   final String username;
   final bool authenticated;
-  User({required this.username, required this.authenticated});
+  final int id;
+  final String email;
+  final String accessToken;
+  User({
+    required this.username,
+    required this.authenticated,
+    required this.id,
+    required this.email,
+    required this.accessToken,
+  });
   factory User.fromJson(Map<String, dynamic> json) {
     return switch (json) {
-      {'authenticated': bool authenticated, 'user': String username} => User(
-        username: username,
-        authenticated: authenticated,
-      ),
+      {
+        'username': String username,
+        'id': int id,
+        'email': String email,
+        'accessToken': String accessToken,
+      } =>
+        User(
+          username: username,
+          authenticated: true,
+          id: id,
+          email: email,
+          accessToken: accessToken,
+        ),
       _ => throw const FormatException('Failed to load User.'),
     };
   }
